668043be4b129e15b0a9beec3ef5f273057897c9
Dennis <244068893@qq.com>
2019-11-25 10:31:38 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -13,3 +13,19 @@
 
 [Visual Paradigm](http://www.visual-paradigm.com)
 一款非常好用的画图软件
+
+
+## 脚本
+```sql
+create table user
+(
+    id           int auto_increment
+        primary key not null ,
+    account_id   varchar(100) null,
+    token        char(36)     null,
+    name         varchar(50)  null,
+    gmt_create   bigint       null,
+    gmt_modified bigint       null
+);
+```
+
--- src/main/java/com/dennis/blog/controller/AuthorizeController.java
+++ src/main/java/com/dennis/blog/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.dennis.blog.controller;
 
 import com.dennis.blog.dto.AccessTokenDTO;
 import com.dennis.blog.dto.GithubUser;
+import com.dennis.blog.mapper.UserMapper;
+import com.dennis.blog.model.User;
 import com.dennis.blog.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,9 +29,14 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id("7ec7112452d3904f4082");
@@ -35,8 +45,23 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            //UUID随机码
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+            //success, write into cookie and session
+        }else{
+            return "redirect:/";
+            //failed
+        }
+
     }
 }
