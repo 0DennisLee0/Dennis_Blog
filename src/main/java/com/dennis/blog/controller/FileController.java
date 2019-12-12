2c2efa5aedf2e9aa5460d2f5773051b72200886e
Dennis <244068893@qq.com>
2019-12-12 15:52:42 +0800

    upload picture has finished

---
+++ src/main/java/com/dennis/blog/controller/FileController.java
@@ -0,0 +1,37 @@
+package com.dennis.blog.controller;
+
+import com.dennis.blog.dto.FileDTO;
+import com.dennis.blog.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Controller
+public class FileController {
+
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        //找到前端传递图片的方式
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+        try {
+            uCloudProvider.upload(file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/miko.jpg");
+        return fileDTO;
+    }
+}
---
+++ src/main/java/com/dennis/blog/provider/UCloudProvider.java
@@ -0,0 +1,60 @@
+package com.dennis.blog.provider;
+
+import cn.ucloud.ufile.UfileClient;
+import cn.ucloud.ufile.api.object.ObjectConfig;
+import cn.ucloud.ufile.auth.ObjectAuthorization;
+import cn.ucloud.ufile.auth.UfileObjectLocalAuthorization;
+import cn.ucloud.ufile.bean.PutObjectResultBean;
+import cn.ucloud.ufile.exception.UfileClientException;
+import cn.ucloud.ufile.exception.UfileServerException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+public class UCloudProvider {
+
+    @Value("${ucloud.ufile.public-key}")
+    private String publicKey;
+
+    @Value("${ucloud.ufile.private-key}")
+    private String ptivateKey;
+
+    public String upload(InputStream fileStream, String mimeType, String fileName) {
+        String generateFileName;
+        String[] filePaths = fileName.split("\\.");
+        if (filePaths.length > 1) {
+            generateFileName = UUID.randomUUID().toString() + "." + filePaths[filePaths.length - 1];
+        }else{
+            return null;
+        }
+
+        try {
+            ObjectAuthorization objectAuthorization = new UfileObjectLocalAuthorization(publicKey, ptivateKey);
+            ObjectConfig config = new ObjectConfig("cn-bj", "ufileos.com");
+            PutObjectResultBean response = UfileClient.object(objectAuthorization, config)
+                    .putObject(fileStream, mimeType)
+                    .nameAs(generateFileName)
+                    .toBucket("abstract-culture")
+                    /**
+                     * 配置进度监听
+                     */
+                    .setOnProgressListener((bytesWritten, contentLength) -> {
+
+                    })
+                    .execute();
+        } catch (UfileClientException e) {
+            e.printStackTrace();
+            return null;
+        } catch (UfileServerException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return "";
+    }
+
+}
